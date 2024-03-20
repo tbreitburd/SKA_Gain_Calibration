db1730a91fe9e4ecbf2cd5c3600ec0f348a3cc57
Thomas Breitburd <tmb76@cam.ac.uk>
2024-03-20 23:59:02 +0000

    Created and wrote code for question 5
    Have the code calculate the EEPs, estimate the gain solutions for each model, then get the array pattern for each feed and model, and finally plot the station beam.

---
+++ harp_beam/question_5.py
@@ -0,0 +1,120 @@
+"""!@file question_5.py
+
+"""
+
+import numpy as np
+from harp_beam import compute_EEPs, StEFCal, compute_array_pattern
+from plot_funcs import plot_station_beam
+import scipy.io
+import os
+
+
+# -------------
+# Load the EEPs
+# -------------
+# Load the EEPs
+data_dir = os.getcwd()
+filename_eep = os.path.join(
+    data_dir, "harp_beam", "data_EEPs_SKALA41_random_100MHz.mat"
+)
+SKA_data = scipy.io.loadmat(filename_eep)
+pos_ant = np.array(SKA_data["pos_ant"])
+x_pos = pos_ant[:, 0]
+y_pos = pos_ant[:, 1]
+
+# Define the angles over which to get EEPs
+# Here we want to get EEps for theta = [ -pi/2, pi/2] and phi = 0
+# Get EEPs for theta from -pi/2 to pi/2
+theta_half = np.linspace(0, np.pi / 2, 500)[:, None]
+theta = np.concatenate(
+    (-theta_half[::-1], theta_half), axis=0
+)  # to get theta from -pi/2 to pi/2
+phi = np.zeros_like(theta)
+
+# Compute the EEPs
+v_theta_polY, v_phi_polY, v_theta_polX, v_phi_polX = compute_EEPs(SKA_data, theta, phi)
+
+# ----------------
+# Compute the EEPs
+# ----------------
+
+# Because compute EEPs returns the spherical components of the EEPs,
+# the components need to be combined to get the EEPs
+
+# Combine the EEPs
+v_polX = np.sqrt(np.abs(v_theta_polX) ** 2 + np.abs(v_phi_polX) ** 2)
+v_polY = np.sqrt(np.abs(v_theta_polY) ** 2 + np.abs(v_phi_polY) ** 2)
+
+
+# ----------------------------------------------------------
+# Get the estimates of the EEPs and AEP model gain solutions
+# ----------------------------------------------------------
+
+# Load the measured and model visibilities, and the exact gain solution
+data_dir = os.getcwd()
+filename_vismat = os.path.join(data_dir, "harp_beam", "data_20feb2024_2330_100MHz.mat")
+mat = scipy.io.loadmat(filename_vismat)
+R = np.array(mat["R"])  # covariance matrix
+M_AEP = np.array(mat["M_AEP"])  # model matrix using AEP
+M_EEPs = np.array(mat["M_EEPs"])  # model matrix using all EEPs
+g_sol = np.array(mat["g_sol"]).reshape(256)  # exact gain solution
+G_true = np.diag(g_sol)
+
+# Set the convergence tolerence and maximum number of iterations, and number of antennas
+tau = 1e-5
+i_max = 100
+P = 256
+
+# Run the SteEFCal algorithm for the AEP and EEPs model matrices
+G_AEP, diff_AEP, abs_error_AEP, amp_diff_AEP, phase_diff_AEP = StEFCal(
+    M_AEP, R, tau, i_max, P, g_sol
+)
+
+G_EEPs, diff_EEPs, abs_error_EEPs, amp_diff_EEPs, phase_diff_EEPs = StEFCal(
+    M_EEPs, R, tau, i_max, P, g_sol
+)
+
+# ----------------------------------------------
+# Get the array pattern to plot the station beam
+# ----------------------------------------------
+
+P_true_X = compute_array_pattern(
+    G_true, v_theta_polX, v_phi_polX, x_pos, y_pos, theta, 0, 0, 0, 100
+)
+P_EEPs_X = compute_array_pattern(
+    G_EEPs, v_theta_polX, v_phi_polX, x_pos, y_pos, theta, 0, 0, 0, 100
+)
+P_AEP_X = compute_array_pattern(
+    G_AEP, v_theta_polX, v_phi_polX, x_pos, y_pos, theta, 0, 0, 0, 100
+)
+P_true_Y = compute_array_pattern(
+    G_true, v_theta_polY, v_phi_polY, x_pos, y_pos, theta, 0, 0, 0, 100
+)
+P_EEPs_Y = compute_array_pattern(
+    G_EEPs, v_theta_polY, v_phi_polY, x_pos, y_pos, theta, 0, 0, 0, 100
+)
+P_AEP_Y = compute_array_pattern(
+    G_AEP, v_theta_polY, v_phi_polY, x_pos, y_pos, theta, 0, 0, 0, 100
+)
+
+
+# Get the patterns in dBV
+P_true_X_dB = 20 * np.log10(np.abs(P_true_X))
+P_EEPs_X_dB = 20 * np.log10(np.abs(P_EEPs_X))
+P_AEP_X_dB = 20 * np.log10(np.abs(P_AEP_X))
+P_true_Y_dB = 20 * np.log10(np.abs(P_true_Y))
+P_EEPs_Y_dB = 20 * np.log10(np.abs(P_EEPs_Y))
+P_AEP_Y_dB = 20 * np.log10(np.abs(P_AEP_Y))
+
+# Plot the station beam
+
+plot_station_beam(
+    theta,
+    P_true_X_dB,
+    P_true_Y_dB,
+    P_EEPs_X_dB,
+    P_EEPs_Y_dB,
+    P_AEP_X_dB,
+    P_AEP_Y_dB,
+    100,
+)
